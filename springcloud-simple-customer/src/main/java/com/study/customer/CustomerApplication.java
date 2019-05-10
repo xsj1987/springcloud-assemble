0dd1959441088646b5b2813d027c26a6f4bcf793
xusj <xusj@13322.com>
2019-05-10 17:58:35 +0800

    注册微服务

--- springcloud-simple-customer/src/main/java/com/study/customer/CustomerApplication.java
+++ springcloud-simple-customer/src/main/java/com/study/customer/CustomerApplication.java
@@ -2,10 +2,15 @@ package com.study.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @EnableDiscoveryClient 该标识用来声明这个一个Eureka Client
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerApplication {
 
 	@Bean
--- springcloud-simple-provider/src/main/java/com/study/provider/ProviderApplication.java
+++ springcloud-simple-provider/src/main/java/com/study/provider/ProviderApplication.java
@@ -2,8 +2,13 @@ package com.study.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @EnableDiscoveryClient 该标识用来声明这个一个Eureka Client
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderApplication {
 
 	public static void main(String[] args) {
